b37ea993b8fc357fa96264f2b5d2c5fc481f1cf7
PushEax <pusheax.vk@gmail.com>
2024-12-20 03:26:50 +0200

    add js for task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColor.addEventListener('click', event => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+});
