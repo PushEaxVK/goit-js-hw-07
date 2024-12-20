2d3b4f0526057342ff92239b647b3d63c34edf13
PushEax <pusheax.vk@gmail.com>
2024-12-20 03:18:54 +0200

    add js for task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+'use strict';
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+  } else {
+    const obj = {
+      email: email.trim(),
+      password: password.trim(),
+    };
+    console.log(obj);
+    form.reset();
+  }
+}
